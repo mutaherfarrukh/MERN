49c8d1af90fa3020dbb53670b74e3f7354c46405
Mutaher Farrukh <mutaherfarrukh@gmail.com>
2021-09-01 16:49:50 -0500

    Added FineArt Project

---
+++ fine-art/src/components/Row.js
@@ -0,0 +1,26 @@
+import React,{Component} from 'react';
+import Pixel from './Pixel';
+
+
+class Row extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="row">
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+                <Pixel />
+            </div>
+        )
+    }
+}
+
+export default Row;
