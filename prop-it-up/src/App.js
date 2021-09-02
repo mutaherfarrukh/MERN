c0ea680c3fc32799358abf55ce0ec321a64ed490
Mutaher Farrukh <mutaherfarrukh@gmail.com>
2021-09-02 15:24:22 -0500

    Added Like Button

--- prop-it-up/src/App.js
+++ prop-it-up/src/App.js
@@ -5,10 +5,10 @@ import PersonCard from './components/PersonCard';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstname = {"Jane"} lastname = {"Doe"} age = {45} haircolor = {"Black"}/>
-      <PersonCard firstname = {"John"} lastname = {"Smith"} age = {88} haircolor = {"Brown"}/>
-      <PersonCard firstname = {"Millard"} lastname = {"Fillmore"} age = {50} haircolor = {"Brown"}/>
-      <PersonCard firstname = {"Maria"} lastname = {"Smith"} age = {62} haircolor = {"Brown"}/>
+      <PersonCard firstname = {"Jane"} lastname = {"Doe"} age = {45} haircolor = {"Black"} likes={0}/>
+      <PersonCard firstname = {"John"} lastname = {"Smith"} age = {88} haircolor = {"Brown"} likes={0}/>
+      <PersonCard firstname = {"Millard"} lastname = {"Fillmore"} age = {50} haircolor = {"Brown"} likes={0}/>
+      <PersonCard firstname = {"Maria"} lastname = {"Smith"} age = {62} haircolor = {"Brown"} likes={0}/>    
     </div>
   );
 }
--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -3,14 +3,23 @@ import React, {Component} from 'react';
 class PersonCard extends Component{
     constructor(props){
         super(props);
+        this.state={
+            "likes" : this.props.likes
+        }
     }
 
     render(){
+        const addLike = () => {
+            this.setState({
+                "likes" : this.state.likes+1
+            })
+        }
         return(
             <div>
                 <h1>{this.props.lastname}, {this.props.firstname}</h1>
                 <p>Age: {this.props.age}</p>
                 <p>Hair Color: {this.props.haircolor}</p>
+                <button onClick={addLike}>Likes: {this.state.likes}</button>
             </div>
         )
     }
