69d028c069b911c8be6a2272be90e5ddf33f57ad
Mutaher Farrukh <mutaherfarrukh@gmail.com>
2021-09-08 18:02:17 -0500

    Added Button to go back to Main Page

--- luke-apiwalker/src/App.js
+++ luke-apiwalker/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Switch, Route, Link} from "react-router-dom";
 import Main from './views/Main';
 import People from './views/People';
 import Planets from './views/Planets';
@@ -9,6 +9,13 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <div id="header">
+
+        <Link className="btn btn-info" to="/">Main</Link>
+        {/* <Link className="btn btn-info" to="/people/1">People</Link>
+        <Link className="btn btn-info" to="/planets/1">Planets</Link> */}
+
+      </div>
       <Switch>
         <Route exact path="/">
           <Main />
